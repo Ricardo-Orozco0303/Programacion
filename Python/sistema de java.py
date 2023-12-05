2333a115456326511f98f4bfc50ba5897365312d
Ricardo.orozco0303 <141371507+Ricardo-Orozco0303@users.noreply.github.com>
2023-12-05 10:47:04 -0500

    Add files via upload

--- Python/sistema de java.py
+++ Python/sistema de java.py
@@ -6,37 +6,31 @@ print("¿Cual es el nombre del trabajador?: ")
 Nombre = (input())
 print("")
 print("¿Cuanto tiempo de servicio tiene el tabajador?: ")
-antiguedad = (input())
+antiguedad = int (input())
 print("")
 print("¿Cules la clave del tabajador ?: ")
 clave = (input())
 print("")
-if clave == 1 :
-    if antiguedad == 1 :
-       print("El trabajador " + Nombre + "tiene derecho a 6 dias de vacaciones")
-    elif antiguedad >= 2 and antiguedad <= 6 :
-       print("El trabajador " + Nombre + "tiene derecho a 14 dias de vacaciones")
+if clave == "1":
+    if antiguedad == 1:
+        print("El trabajador " + Nombre + " tiene derecho a 6 días de vacaciones")
+    elif 2 <= antiguedad <= 6:
+        print("El trabajador " + Nombre + " tiene derecho a 14 días de vacaciones")
     elif antiguedad >= 7:
-       print("El trabajador " + Nombre + "tiene derecho a 20 dias de vacaciones")
-     	
-    if clave == 2 :
-        if antiguedad == 1:
-            print("El trabajador " + Nombre + "tiene derecho a 7 dias de vacaciones")
-        elif antiguedad >= 2 and antiguedad <= 6 :
-            print("El trabajador " + Nombre + "tiene derecho a 15 dias de vacaciones")
-        elif antiguedad >= 7 :
-            print("El trabajador " + Nombre + "tiene derecho a 22 dias de vacaciones")
-    
+        print("El trabajador " + Nombre + " tiene derecho a 20 días de vacaciones")
 
-    else if (clave == 3)
+elif clave == "2":
+    if antiguedad == 1:
+        print("El trabajador " + Nombre + " tiene derecho a 7 días de vacaciones")
+    elif 2 <= antiguedad <= 6:
+        print("El trabajador " + Nombre + " tiene derecho a 15 días de vacaciones")
+    elif antiguedad >= 7:
+        print("El trabajador " + Nombre + " tiene derecho a 22 días de vacaciones")
 
-      if(antiguedad == 1){
-       System.out.printl("Eltrabajador " + nombre + "tiene derecho a 10 dias de vacaciones");
-     } else if(antiguedad >= 2 and antiguedad <= 6){
-       System.out.printl("Eltrabajador " + nombre + "tiene derecho a 20 dias de vacaciones");
-     } else if(antiguedad >= 7){
-       System.out.printl("Eltrabajador " + nombre + "tiene derecho a 30 dias de vacaciones");
-     }
-   
-    else 
-    System.out.printl("Error la clave de departamento es incorrecta ");
+elif clave == "3":
+    if antiguedad == 1:
+        print("El trabajador " + Nombre + " tiene derecho a 10 días de vacaciones")
+    elif 2 <= antiguedad <= 6:
+        print("El trabajador " + Nombre + " tiene derecho a 20 días de vacaciones")
+    elif antiguedad >= 7:
+        print("El trabajador " + Nombre + " tiene derecho a 30 días de vacaciones")
