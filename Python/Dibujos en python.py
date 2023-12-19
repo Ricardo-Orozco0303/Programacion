bb0e0cd57cfb00c4463b0f74c2e4a7f706d3e102
Ricardo.orozco0303 <141371507+Ricardo-Orozco0303@users.noreply.github.com>
2023-12-18 19:13:35 -0500

    Add files via upload

--- Python/Dibujos en python.py
+++ Python/Dibujos en python.py
@@ -1,23 +1,51 @@
 import turtle
+import colorsys as cs
+import pygame 
 
-ventana = turtle.Screen()
-mi_tortuga = turtle.Turtle()
+def draw_pattern():
+    for j in range(25):
+        for i in range(25):
+            # Obtener valores HSV y ajustarlos para que estén dentro del rango (0, 1)
+            hue = i / 25
+            saturation = max(0.5, min(1, j / 25))  # Aumentar la saturación para colores más intensos
+            value = 0.9  # Aumentar la luminosidad para colores más intensos
 
-# Dibujar el corazón
-mi_tortuga.left(50)          # Girar la tortuga a la izquierda en un ángulo de 50 grados
+            # Utilizar tonalidades asociadas con colores femeninos
+            hue = (hue + 0.2) % 1.0  # Desplazar el tono para obtener colores diferentes
 
-# Dibujar la parte superior del corazón
-mi_tortuga.forward(133)      # Mover hacia adelante 133 unidades
-mi_tortuga.circle(50, 200)   # Dibujar un arco de círculo con radio 50 y ángulo de 200 grados
+            turtle.color(cs.hsv_to_rgb(hue, saturation, value))
+            turtle.right(90)
+            turtle.circle(200 - j * 4, 90)
+            turtle.left(90)
+            turtle.circle(200 - j * 4, 90)
+            turtle.right(180)
+            turtle.circle(50, 24)
 
-# Girar para dibujar la parte inferior del corazón
-mi_tortuga.right(140)        # Girar a la derecha en un ángulo de 140 grados
-mi_tortuga.circle(50, 200)   # Dibujar otro arco de círculo con las mismas características
-mi_tortuga.forward(133)      # Mover hacia adelante para completar la forma del corazón
+def draw_love_message():
+    turtle.penup()
+    turtle.color("deeppink")  # Utilizar un tono de rosa intenso para el mensaje
+    turtle.goto(0, 50)
+    turtle.pendown()
+    turtle.write("¡Te amo!", align="center", font=("Arial", 16, "normal"))
 
-mi_tortuga.penup()  # Levantar el lápiz para no dejar rastro
-mi_tortuga.goto(0, 100)  # Mover la tortuga al centro del cuadrado
-mi_tortuga.pendown()  # Bajar el lápiz para empezar a escribir
-mi_tortuga.write("¡Te amo!", align="center", font=("Arial", 16, "normal"))
-mi_tortuga.hideturtle()
-ventana.exitonclick()
+def main():
+    turtle.setup(600, 600)
+    turtle.speed(0.100)
+    turtle.tracer(10)
+    turtle.width(2)
+    turtle.bgcolor("black")  # Fondo de negro 
+
+    turtle.penup()
+    turtle.goto(0, 0)
+    turtle.pendown()
+    draw_pattern()
+
+    draw_love_message()
+
+    turtle.hideturtle()
+    turtle.done()
+if __name__ == "__main__":
+    main()
+
+
+
